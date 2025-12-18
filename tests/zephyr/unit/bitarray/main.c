41d1618f468d2af90cc2e9356fed71fb1b54872d
Flavio Ceolin <flavio@hubble.com>
2025-12-18 15:45:09 -0800

    WIP
    Signed-off-by: Flavio Ceolin <flavio@hubble.com>

--- tests/zephyr/unit/bitarray/main.c
+++ tests/zephyr/unit/bitarray/main.c
@@ -35,9 +35,6 @@ ZTEST(bitarray, test_invalid_access)
 	hubble_bitarray_init(&bit_array);
 
 	zassert_equal(hubble_bitarray_get_bit(&bit_array, 1), -EINVAL);
-	zassert_equal(hubble_bitarray_get_bit(&bit_array, INT_MAX), -EINVAL);
-	zassert_ok(hubble_bitarray_append(&bit_array, &data, sizeof(data) * 8));
-	zassert_equal(hubble_bitarray_get_bit(&bit_array, INT_MAX), -EINVAL);
 }
 
 ZTEST(bitarray, test_regular_usage)
